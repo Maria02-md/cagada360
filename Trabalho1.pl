9de422dcca2115ab30dc8de82c888eaa343dc956
JeniferConstantino <58510995+JeniferConstantino@users.noreply.github.com>
2019-12-06 14:07:29 +0000

    Update Trabalho1.pl

--- Trabalho1.pl
+++ Trabalho1.pl
@@ -1,61 +1,53 @@
-﻿/*Exercicio 1:*/
-
-/*L¬,∧,∨,→*/
-
 :-op(100, fy, 'neg').
-:-op(200, xfy,'e').
-:-op(300, xfy,'ou').
-:-op(400, xfy,'imp').
+:-op(200, xfy, 'e').
+:-op(300, xfy, 'ou').
+:-op(400, xfy, 'imp').
 
-/*tem o valor verdadeiro se N ´e
-um n´umero natural (positivo) e X ´e o elemento que est´a na posi¸c˜ao
+/*NOTAS
+{p, p → q, q} é representado da seguinte forma: [p, p imp q, q].
+SIMBOLO PROPOSICIONAL: p,q, ou seja, são simvolos simples
+LITERAL: ~q, q, ou seja, é o simbolo proposicional ou a negação desse
+         simbolo.
+*/
+
+
+/*Execicio1:
+   F-formula de L¬,∧,∨,→
+   X-simbolo proposicional
+   S-lista de simbolos proposicionais
+   L-lista de 0's e 1's
+   V-valoração da fomula
+*/
+
+
+/*tem o valor verdadeiro se N é
+um número natural (positivo) e X é o elemento que está na posiç˜ao
 N da lista L*/
 enesimo(1,[X|L],X).
 enesimo(N,[X|L],Y):-enesimo(N1,L,Y), N is N1+1.
 
-/*F- Formula de L¬,∧,∨,→*
-  X-simbolo proposicional
-  S-lista de simbolos proposicionais
-  L-lista de 0's e 1's
-  O calc_valor serve para obter as valorações da formula F
-*/
-calc_valor(F,S,L,V):-enesimo(N,S,F),enesimo(N,L,V).
-calc_valor(neg X,S,L,0):-calc_valor(X,S,L,1).
-calc_valor(neg X,S,L,1):-calc_valor(X,S,L,0).
-calc_valor(X imp Y,S,L,0):-calc_valor(X,S,L,1),calc_valor(Y,S,L,0).
-calc_valor(X imp Y,S,L,1):-calc_valor(X,S,L,0).
-calc_valor(X imp Y,S,L,1):-calc_valor(Y,S,L,1).
-calc_valor(X e Y,S,L,1):-calc_valor(X,S,L,1),calc_valor(Y,S,L,1).
-calc_valor(X e Y,S,L,0):-calc_valor(X,S,L,0).
-calc_valor(X e Y,S,L,0):-calc_valor(Y,S,L,0).
-calc_valor(X ou Y,S,L,1):-calc_valor(X,S,L,1).
-calc_valor(X ou Y,S,L,1):-calc_valor(Y,S,L,1).
-calc_valor(X ou Y,S,L,0):-calc_valor(X,S,L,0),calc_valor(Y,S,L,0).
 
-concatena([],L,L).
-concatena([X|R],L,[X|S]):-concatena(R,L,S).
+valor_log(F,S,L,V):-enesimo(N,S,F),enesimo(N,L,V). /*Se F é o elemento que esta na posição N da lista S, então o valor lógico de F é o valor que esta na posição N da lista L*/
+valor_log(neg X,S,L,0):-valor_log(X,S,L,1).
+valor_log(neg X,S,L,1):-valor_log(X,S,L,0).
+valor_log(X imp Y,S,L,0):-valor_log(X,S,L,1),valor_log(Y,S,L,0).
+valor_log(X imp Y,S,L,1):-valor_log(X,S,L,0).
+valor_log(X imp Y,S,L,1):-valor_log(Y,S,L,1).
+valor_log(X e Y,S,L,1):-valor_log(X,S,L,1),valor_log(Y,S,L,1).
+valor_log(X e Y,S,L,0):-valor_log(X,S,L,0).
+valor_log(X e Y,S,L,0):-valor_log(Y,S,L,0).
+valor_log(X ou Y,S,L,1):-valor_log(X,S,L,1).
+valor_log(X ou Y,S,L,1):-valor_log(Y,S,L,1).
+valor_log(X ou Y,S,L,0):-valor_log(X,S,L,0),valor_log(Y,S,L,0).
 
-literal(F):-simb_prop(F).
-literal(neg F):- simb_prop(F).
 
-simb_prop(F):-not(F= neg X), not(F = X e Y), not(F= X ou Y),not(F=X imp Y).
-simb_prop(neg A, A):- literal(neg A).
+val_sat_list_form(F,S,V):-enesimo(N,S,F),enesimo(N,F,V).
+val_sat_list_form(F,S,V):- valor_log(F,S,V,1).
 
-simb_prop(A e B, [A|B],L3,L4):- simb_prop(A),simb_prop(B),concatena([A|B],L3,L4).
-simb_prop(A e B, [B|S]):- not(simb_prop(A)),simb_prop(B),simb_prop(A,[B|S]).
-simb_prop(A e B, [A|S]):- simb_prop(A),not(simb_prop(B)),simb_prop(B,[A|S]).
-simb_prop(A e B, S):- not(simb_prop(A)),not(simb_prop(B)),simb_prop(A,S),simb_prop(B,S).
 
-simb_prop(A imp B, [A|B]):-simb_prop(A),simb_prop(B).
-simb_prop(A imp B, [B|S]):- not(simb_prop(A)),simb_prop(B),simb_prop(A,[B|S]).
-simb_prop(A imp B, [A|S]):- simb_prop(A),not(simb_prop(B)),simb_prop(B,[A|S]).
-simb_prop(A imp B, S):- not(simb_prop(A)),not(simb_prop(B)),simb_prop(A,S),simb_prop(B,S).
-
-simb_prop(A ou B, [A|B]):-simb_prop(A),simb_prop(B).
-simb_prop(A ou B, [B|S]):- not(simb_prop(A)),simb_prop(B),simb_prop(A,[B|S]).
-simb_prop(A ou B, [A|S]):- simb_prop(A),not(simb_prop(B)),simb_prop(B,[A|S]).
-simb_prop(A ou B, S):- not(simb_prop(A)),not(simb_prop(B)),simb_prop(A,S),simb_prop(B,S).
+val_sat_list_form2([],S,V).
+val_sat_list_form2([F|T],S,V):-val_sat_list_form(F,S,V),val_sat_list_form2(T,S,V).
 
 
 
-exercicio1([F|R],L2):-
+
