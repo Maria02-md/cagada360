f1a190a71e33269c12ac776d44b462fec728b5a9
melissafreitas <58570288+melissafreitas@users.noreply.github.com>
2019-12-09 22:00:13 +0000

    Update Trabalho1.pl

--- Trabalho1.pl
+++ Trabalho1.pl
@@ -49,5 +49,29 @@ val_sat_list_form2([],S,V).
 val_sat_list_form2([F|T],S,V):-val_sat_list_form(F,S,V),val_sat_list_form2(T,S,V).
 
 
+acrescenta(X,L,[X|L]).
+
+simb_prop(P):-not(P=neg X),not(P=X e Y),not(P=X ou Y),not(P=X imp Y).
+literal(X):-simb_prop(X).
+literal(neg X):-simb_prop(X).
+implica(X imp Y):-literal(X),literal(Y).
+conj(X e Y):-literal(X),literal(Y).
+disj(X ou Y):-literal(X),literal(Y).
+
+concatena([],L,L).
+concatena([X|R],L,[X|S]):-concatena(R,L,S).
+inverter([],[]).
+inverter([X|R],L):-inverter(R,P),concatena(P,[X],L). % escrevi o inverte porque a lista que obtinha no output de porcaria1 era invertida %
+
+
+porcaria1([],L):-inverter(L,R),escreve_lista([R]).
+porcaria1([X1|X2],L):-simb_prop(X1),acrescenta(X1,L,[X1|L]),porcaria1(X2,[X1|L]).
+porcaria1([neg X1|X2],L):-simb_prop(X1),acrescenta(X1,L,[X1|L]),porcaria1(X2,[X1|L]).
+porcaria1([X1 e _|X2],L):-simb_prop(X1),acrescenta(X1,L,[X1|L]),porcaria1(X2,[X1|L]).
+porcaria1([_ e Y|X2],L):-simb_prop(Y),acrescenta(Y,L,[Y,L]),porcaria1(X2,[Y,L]).
+porcaria1([X1 e Y|X2],L):-conj(X1 e Y),acrescenta([X1,Y],L,[Y|[X1,L]]),porcaria1(X2,[Y|[X1,L]]). %ainda não funciona, só devolve o primeiro%
+
+
+porcaria1([X1 imp Y|X2],L):-implica(X1 imp Y),acrescenta(X1,L,[X1|L]),acrescenta(Y,[X1|L],[Y|[X1|L]]),porcaria1(X2,[Y|[X1,L]]). %funciona mais ou menos%
 
 
