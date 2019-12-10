898aa8c500ec5a873900b791ec70e268c6301fb3
JeniferConstantino <58510995+JeniferConstantino@users.noreply.github.com>
2019-12-10 10:39:54 +0000

    Update Trabalho1.pl

--- Trabalho1.pl
+++ Trabalho1.pl
@@ -20,6 +20,31 @@ LITERAL: ~q, q, ou seja, é o simbolo proposicional ou a negação desse
 */
 
 
+
+
+simb_prop(P):- not(P = neg  X),not(P = X  e  Y), not(P = X ou Y), not(P = X imp Y).
+literal( X ):- simb_prop(X) .
+literal( neg X ):- simb_prop(X) .
+implicações( X  imp  Y ):- literal(X),literal(Y).
+conj(X e Y):-literal(X),literal(Y).
+disj(X ou Y):-literal(X),literal(Y).
+
+concatena([],L,L).
+concatena([X|R],L,[X|S]):-concatena(R,L,S) .
+
+lista_s1([X1],[X1]):-simb_prop(X1).
+lista_s1([neg X1],[X1]):-simb_prop(X1).
+lista_s1([X1 e X2],[X1,X2]):-simb_prop(X1),simb_prop(X2).
+lista_s1([X1 ou X2],[X1,X2]):-simb_prop(X1),simb_prop(X1).
+lista_s1([X1 imp X2],[X1,X2]):- simb_prop(X1),simb_prop(X2).
+lista_s1([X1|F],L):-simb_prop(X1),lista_s1(F,L2),concatena([X1],L2,L).
+lista_s1([neg X1|F],L):-simb_prop(X1),lista_s1(F,L2),concatena([X1],L2,L).
+lista_s1([X1 e X2|F],L):-simb_prop(X1),simb_prop(X2),lista_s1(F,L2),concatena([X1,X2],L2,L).
+lista_s1([X1 ou X2|F],L):-simb_prop(X1),simb_prop(X2),lista_s1(F,L2),concatena([X1,X2],L2,L).
+lista_s1([X1 imp X2|F],L):-simb_prop(X1),simb_prop(X2),lista_s1(F,L2),concatena([X1,X2],L2,L).
+
+
+
 /*tem o valor verdadeiro se N é
 um número natural (positivo) e X é o elemento que está na posiç˜ao
 N da lista L*/
@@ -41,37 +66,26 @@ valor_log(X ou Y,S,L,1):-valor_log(Y,S,L,1).
 valor_log(X ou Y,S,L,0):-valor_log(X,S,L,0),valor_log(Y,S,L,0).
 
 
-val_sat_list_form(F,S,V):-enesimo(N,S,F),enesimo(N,F,V).
+/*val_sat_list_form(F,S,V):-enesimo(N,S,F),enesimo(N,F,V).ACABEI DE REPARAR QUE ESTA LINHA DE CODIGO NÃO ESTA AQUI A FAZER NADA*/
 val_sat_list_form(F,S,V):- valor_log(F,S,V,1).
-
-
 val_sat_list_form2([],S,V).
 val_sat_list_form2([F|T],S,V):-val_sat_list_form(F,S,V),val_sat_list_form2(T,S,V).
 
 
-acrescenta(X,L,[X|L]).
-
-simb_prop(P):-not(P=neg X),not(P=X e Y),not(P=X ou Y),not(P=X imp Y).
-literal(X):-simb_prop(X).
-literal(neg X):-simb_prop(X).
-implica(X imp Y):-literal(X),literal(Y).
-conj(X e Y):-literal(X),literal(Y).
-disj(X ou Y):-literal(X),literal(Y).
-
-concatena([],L,L).
-concatena([X|R],L,[X|S]):-concatena(R,L,S).
-inverter([],[]).
-inverter([X|R],L):-inverter(R,P),concatena(P,[X],L). % escrevi o inverte porque a lista que obtinha no output de porcaria1 era invertida %
 
 
-porcaria1([],L):-inverter(L,R),escreve_lista([R]).
-porcaria1([X1|X2],L):-simb_prop(X1),acrescenta(X1,L,[X1|L]),porcaria1(X2,[X1|L]).
-porcaria1([neg X1|X2],L):-simb_prop(X1),acrescenta(X1,L,[X1|L]),porcaria1(X2,[X1|L]).
-porcaria1([X1 e _|X2],L):-simb_prop(X1),acrescenta(X1,L,[X1|L]),porcaria1(X2,[X1|L]).
-porcaria1([_ e Y|X2],L):-simb_prop(Y),acrescenta(Y,L,[Y,L]),porcaria1(X2,[Y,L]).
-porcaria1([X1 e Y|X2],L):-conj(X1 e Y),acrescenta([X1,Y],L,[Y|[X1,L]]),porcaria1(X2,[Y|[X1,L]]). %ainda não funciona, só devolve o primeiro%
+/*
+list_vals_sat_list_form([F],S,R):-val_sat_list_form(F,S,R),write(R).
+list_vals_sat_list_form2([F|T],S,R):-val_sat_list_form(F,S,R),list_vals_sat_list_form2(T,S,R).
+*/
 
 
-porcaria1([X1 imp Y|X2],L):-implica(X1 imp Y),acrescenta(X1,L,[X1|L]),acrescenta(Y,[X1|L],[Y|[X1|L]]),porcaria1(X2,[Y|[X1,L]]). %funciona mais ou menos%
+
+
+
+
+
+
+
 
 
